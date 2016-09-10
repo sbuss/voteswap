faa03ceedb826b5c9dfbff6119a6f2f03426ed7e
Steven Buss <steven.buss@gmail.com>
2016-09-10 15:03:06 -0700

    Make startcontainers.sh a little smarter and quieter

--- Makefile
+++ Makefile
@@ -10,13 +10,13 @@ setup: lib
 	pip install -r requirements-local.txt
 
 .PHONY: setupdb
-setupdb: startcontainer
+setupdb: deps
 	$(PYTHONPATH) python manage.py migrate
 	$(PYTHONPATH) python manage.py createsuperuser
 	$(PYTHONPATH) python manage.py dumpdata
 
 .PHONY: startcontainer
-startcontainer: stop
+startcontainer:
 	./startcontainers.sh
 
 .PHONY: deps
@@ -24,5 +24,5 @@ deps: startcontainer
 
 .PHONY: stop
 stop:
-	-docker stop mysql-voteswap
-	-docker rm mysql-voteswap
+	-docker stop voteswap-mysql
+	-docker rm voteswap-mysql
--- startcontainers.sh
+++ startcontainers.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-docker run \
-    --name mysql-voteswap \
-    --publish 3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=rootpassword \
-    -e MYSQL_USER=voteswap \
-    -e MYSQL_PASSWORD=password \
-    -v $(pwd)/.mysql_data:/var/lib/mysql \
-    -v $(pwd)/mysqlbootstrap:/docker-entrypoint-initdb.d \
-    -d \
-    mysql:5.7
+CONTAINER_NAME=voteswap-mysql
+MYSQL_USER=voteswap
+MYSQL_PASSWORD=password
 
-docker exec mysql-voteswap sh -c "exec mysql -uroot -prootpassword -e 'show databases;'";
+if [[ $(docker inspect -f {{.State.Running}} $CONTAINER_NAME 2&> /dev/null) != "true" ]]; then
+    echo "Starting ${CONTAINER_NAME}";
+    docker run \
+        --name ${CONTAINER_NAME} \
+        --publish 53306:3306 \
+        -e MYSQL_ROOT_PASSWORD=rootpassword \
+        -e MYSQL_USER=${MYSQL_USER} \
+        -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
+        -v $(pwd)/.mysql_data:/var/lib/mysql \
+        -v $(pwd)/mysqlbootstrap:/docker-entrypoint-initdb.d \
+        -d \
+        mysql:5.7
+fi
+
+connect() {
+    docker exec $CONTAINER_NAME sh -c \
+        "exec mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} -e 'show databases;'" &> /dev/null
+}
+
+connect;
 available=$?;
 until [ $available -eq 0 ]; do
+    echo "Waiting for ${CONTAINER_NAME} to come up"
     sleep 1;
-    docker exec mysql-voteswap sh -c "exec mysql -uroot -prootpassword -e 'show databases;'";
+    connect;
     available=$?;
 done
+
+echo "${CONTAINER_NAME} is running"
