452ddbb2cf6401a08fe6bd0947d13def8f0c6dd1
Steven Buss <steven.buss@gmail.com>
2016-09-11 13:42:47 -0700

    Add bootstrap to static files

--- voteswap/settings.py
+++ voteswap/settings.py
@@ -149,5 +149,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'vendor_static')
+]
+
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
