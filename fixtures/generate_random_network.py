e442d59dc58f7d6eeea3a3786d806af07b2ccb6a
Steven Buss <steven.buss@gmail.com>
2016-09-13 22:03:37 -0700

    Add todo to fix randomness

--- fixtures/generate_random_network.py
+++ fixtures/generate_random_network.py
@@ -7,6 +7,8 @@ RANDOM_SEED = 1024
 
 
 def create_profiles(num_profiles):
+    # This seed does nothing because I'm using fake-factory instead of
+    # factory.fuzzy. TODO fix it
     fuzzy.reseed_random(RANDOM_SEED)
     return [user.profile
             for user in (UserFactory.create() for x in range(num_profiles))]
