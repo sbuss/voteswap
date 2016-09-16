64b2df15db783cfde5ecaff8ba04e46f386a9dc4
Steven Buss <steven.buss@gmail.com>
2016-09-15 21:26:20 -0700

    Add a LandingPageForm with state and candidate choices

---
+++ voteswap/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from polling.models import CANDIDATES
+from polling.models import CANDIDATES_THIRD_PARTY
+from polling.models import STATES
+
+
+class LandingPageForm(forms.Form):
+    state = forms.ChoiceField(choices=STATES)
+    preferred_candidate = forms.ChoiceField(choices=CANDIDATES)
+    second_candidate = forms.ChoiceField(choices=CANDIDATES_THIRD_PARTY)
+    reason = forms.Textarea()
