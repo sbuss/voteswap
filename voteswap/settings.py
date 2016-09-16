b4271ec9c3e359974bf42be962534b8b72ff0016
Steven Buss <steven.buss@gmail.com>
2016-09-15 21:02:45 -0700

    Navbar

--- voteswap/settings.py
+++ voteswap/settings.py
@@ -155,7 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'vendor_static')
+    os.path.join(BASE_DIR, 'vendor_static'),
+    os.path.join(BASE_DIR, 'voteswap', 'static')
 ]
 
 STATIC_ROOT = 'static'
