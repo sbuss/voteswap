a7fe4c0405b3015d982c17860d214ceab75d15cb
Steven Buss <steven.buss@gmail.com>
2016-09-17 14:49:50 -0700

    Sign up form creates Profile objects

--- voteswap/forms.py
+++ voteswap/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from polling.models import CANDIDATES
 from polling.models import CANDIDATES_THIRD_PARTY
 from polling.models import STATES
+from users.models import Profile
 
 
 class LandingPageForm(forms.Form):
@@ -12,3 +13,18 @@ class LandingPageForm(forms.Form):
         choices=CANDIDATES_THIRD_PARTY,
         required=False)
     reason = forms.Textarea()
+
+    def save(self, user):
+        """Create a Profile for the given user
+
+        Args:
+            user: The django User object to associate with the profile
+        Returns the created Profile object
+        """
+        profile = Profile.objects.create(
+            user=user,
+            state=self.cleaned_data['state'],
+            preferred_candidate=self.cleaned_data['preferred_candidate'],
+            second_candidate=self.cleaned_data.get('second_candidate', ''),
+            reason=self.cleaned_data.get('reason', ''))
+        return profile
