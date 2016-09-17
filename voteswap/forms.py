03ed1d6095632ba4e9bd6e75d3b2bb7e26e3de85
Steven Buss <steven.buss@gmail.com>
2016-09-17 16:22:55 -0700

    Fix bugs in LandingPageForm

--- voteswap/forms.py
+++ voteswap/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 from polling.models import CANDIDATES
-from polling.models import CANDIDATES_THIRD_PARTY
+from polling.models import CANDIDATES_MAIN
 from polling.models import STATES
+from polling.models import State
 from users.models import Profile
 
 
@@ -10,9 +11,23 @@ class LandingPageForm(forms.Form):
     state = forms.ChoiceField(choices=STATES)
     preferred_candidate = forms.ChoiceField(choices=CANDIDATES)
     second_candidate = forms.ChoiceField(
-        choices=CANDIDATES_THIRD_PARTY,
+        choices=CANDIDATES_MAIN,
         required=False)
     reason = forms.Textarea()
+
+    def clean(self):
+        """Validate that second_candidate is supplied if swing state."""
+        cleaned_data = super(LandingPageForm, self).clean()
+        state = cleaned_data.get('state')
+        preferred_candidate = cleaned_data.get('preferred_candidate')
+        second_candidate = cleaned_data.get('second_candidate')
+        if state and State.objects.get(name=state).tipping_point_rank > -1:
+            # Convert CANDIDATES_MAIN to a dict because it's a list of tuples
+            if preferred_candidate not in dict(CANDIDATES_MAIN):
+                if second_candidate == "":
+                    raise forms.ValidationError(
+                        "You're in a swing state and selected a third party "
+                        "candidate. You must also select a second choice.")
 
     def save(self, user):
         """Create a Profile for the given user
