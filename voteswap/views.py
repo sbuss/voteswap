79ec03bc6bef3fe8e87d88b4726723ba88dd447a
Steven Buss <steven.buss@gmail.com>
2016-09-19 22:55:27 -0700

    Fetch user's friends

--- voteswap/views.py
+++ voteswap/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponseServerError
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+import requests
 
+from users.models import Profile
 from voteswap.match import get_friend_matches
 # from voteswap.match import NoMatchNecessary
 from voteswap.forms import LandingPageForm
@@ -44,8 +46,31 @@ def landing_page(request):
                               context_instance=context)
 
 
+def _get_friends_for_user(user):
+    """Get the user's facebook friends and get or create their Profiles."""
+    social_user = user.social_auth.get()
+    url = ('https://graph.facebook.com/{uid}/'
+           'friends?fields=id,name,email',
+           '&access_token={token}'.format(
+               uid=social_user.uid,
+               token=social_user.extra_data['access_token'],
+           ))
+    response = requests.get(url)
+    try:
+        data = response.json()["data"]
+    except Exception:
+        data = []
+    friend_profiles = []
+    for friend in data:
+        profile, created = Profile.objects.get_or_create(
+            fb_id=friend['id'], fb_name=friend['name'])
+        friend_profiles.append(profile)
+    user.profile.friends.add(*friend_profiles)
+
+
 @login_required
 def confirm_signup(request):
+    # This happens after logging in, now we can get their friends
     data = request.session.get('landing_page_form', None)
     if not data:
         return HttpResponseRedirect(reverse('signup'))
@@ -55,6 +80,7 @@ def confirm_signup(request):
     try:
         if form.is_valid():
             form.save(request.user)
+            _get_friends_for_user(request.user)
             return HttpResponseRedirect(reverse('users:profile'))
     except Exception as e:
         return HttpResponseServerError("Signup failed: %s" % e)
