426fd800451d3a9077acfeb89d67b9a76c5544c0
Steven Buss <steven.buss@gmail.com>
2016-09-19 18:48:52 -0700

    Print via <friend> for foafs

--- voteswap/tests/test_match_view.py
+++ voteswap/tests/test_match_view.py
@@ -38,4 +38,15 @@ class TestSwingStateMatch(_TestSwingStateMatchBase, Common.Base):
 
 class TestSafeStateFriendsOfFriendsMatch(
         _TestSafeStateFriendsOfFriendsMatchBase, Common.Base):
-    pass
+    def test_through_friend_shown(self):
+        request = self.request.get(reverse(match_view))
+        request.user = self.user
+        response = match_view(request)
+        self.assertEqual(response.status_code, HTTP_OK)
+        matches = get_friend_matches(self.user)
+        for match in matches:
+            self.assertContains(
+                response, match.profile.fb_name)
+            if not match.is_direct:
+                self.assertContains(
+                    response, "via %s" % match.through.fb_name)
