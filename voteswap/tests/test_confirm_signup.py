aafd91efb479463961d439f5ebee99f58c72846b
Steven Buss <steven.buss@gmail.com>
2016-09-20 21:17:45 -0700

    Add tests of _add_facebook_friends_for_user

--- voteswap/tests/test_confirm_signup.py
+++ voteswap/tests/test_confirm_signup.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test import RequestFactory
 from django.test import TestCase
-import json
-from mock import MagicMock
 from mock import patch
 
 from polling.models import CANDIDATE_CLINTON
@@ -12,22 +10,12 @@ from users.models import Profile
 from users.tests.factories import ProfileFactory
 from users.tests.factories import UserFactory
 from voteswap.views import confirm_signup
+from voteswap.tests.test_load_facebook_friends import \
+    _load_mock_request_response
 
 HTTP_OK = 200
 HTTP_REDIRECT = 302
 HTTP_SERVER_ERROR = 500
-
-
-def _load_mock_request_response():
-    with open('voteswap/tests/_fake_friends_list.json', 'r') as f:
-        data = json.load(f)
-
-    def _mock_requests_call(url):
-        # MagicMock that returns the json for .json()
-        retval = MagicMock()
-        retval.json = MagicMock(return_value=data)
-        return retval
-    return _mock_requests_call
 
 
 @patch('voteswap.views.requests.get',
