c6052276024b290dfc3b5ab8d20e5c59a98d13c5
Steven Buss <steven.buss@gmail.com>
2016-09-21 21:20:39 -0700

    Add a FriendMatchContext and proposal_string, and test it

--- users/tests/test_profile_context.py
+++ users/tests/test_profile_context.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 
+from polling.models import CANDIDATE_CLINTON
+from polling.models import CANDIDATE_JOHNSON
+from polling.tests.factories import StateFactory
 from users.models import PairProposal
 from users.tests.factories import ProfileFactory
+from users.tests.factories import UserFactory
 from users.views import ProfileContext
 
 
@@ -16,3 +20,45 @@ class TestProfileContext(TestCase):
             from_profile=profile, to_profile=friend)
         ctx = ProfileContext(profile)
         self.assertTrue(ctx.has_proposed_to_friend(friend))
+
+    def test_swing_match(self):
+        swing_state = StateFactory.create(tipping_point_rank=1)
+        safe_state = StateFactory.create(safe_rank=1)
+        # The logged in user is in a swing state, their match in a safe state
+        user = UserFactory.create(
+            profile__state=swing_state.name,
+            profile__preferred_candidate=CANDIDATE_JOHNSON,
+            profile__second_candidate=CANDIDATE_CLINTON)
+        friend = UserFactory.create(
+            profile__state=safe_state.name,
+            profile__preferred_candidate=CANDIDATE_CLINTON)
+        user.profile.friends.add(friend.profile)
+        profile_ctx = ProfileContext(user.profile)
+        [friend_context] = profile_ctx.good_potential_matches
+        self.assertTrue(
+            "Will vote for %s in %s" % (CANDIDATE_JOHNSON, safe_state.name)
+            in friend_context.proposal_string)
+        self.assertTrue(
+            "your vote for %s in %s" % (CANDIDATE_CLINTON, swing_state.name)
+            in friend_context.proposal_string)
+
+    def test_safe_match(self):
+        swing_state = StateFactory.create(tipping_point_rank=1)
+        safe_state = StateFactory.create(safe_rank=1)
+        # The logged in user is in a safe state, their match in a swing state
+        friend = UserFactory.create(
+            profile__state=swing_state.name,
+            profile__preferred_candidate=CANDIDATE_JOHNSON,
+            profile__second_candidate=CANDIDATE_CLINTON)
+        user = UserFactory.create(
+            profile__state=safe_state.name,
+            profile__preferred_candidate=CANDIDATE_CLINTON)
+        user.profile.friends.add(friend.profile)
+        profile_ctx = ProfileContext(user.profile)
+        [friend_context] = profile_ctx.good_potential_matches
+        self.assertTrue(
+            "Will vote for %s in %s" % (CANDIDATE_CLINTON, swing_state.name)
+            in friend_context.proposal_string)
+        self.assertTrue(
+            "your vote for %s in %s" % (CANDIDATE_JOHNSON, safe_state.name)
+            in friend_context.proposal_string)
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.functional import cached_property
 
+from polling.models import State
 from voteswap.match import get_friend_matches
 
 
@@ -18,7 +19,56 @@ class ProfileContext(object):
 
     @cached_property
     def good_potential_matches(self):
-        return get_friend_matches(self.profile)
+        return [FriendMatchContext(fm, self)
+                for fm in get_friend_matches(self.profile)]
+
+
+class FriendMatchContext(object):
+    def __init__(self, friend_match, profile_context):
+        self.friend_match = friend_match
+        self.profile_context = profile_context
+
+    @property
+    def name(self):
+        profile = self.friend_match.profile
+        try:
+            return profile.user.get_full_name()
+        except:
+            pass
+        return profile.email
+
+    @property
+    def has_been_proposed_to(self):
+        return self.profile_context.has_proposed_to_friend(
+            self.friend_match.profile)
+
+    @property
+    def proposal_string(self):
+        profile = self.friend_match.profile
+        state = State.objects.get(name=profile.state)
+        if state.is_swing:
+            # If I'm a good match, then I must be voting for a third party
+            # and I must have a second choice candidate
+            return (
+                "Will vote for {second_candidate} in {state} in exchange for "
+                "your vote for {preferred_candidate} in {your_state}").format(
+                    second_candidate=profile.second_candidate,
+                    state=profile.state,
+                    preferred_candidate=profile.preferred_candidate,
+                    your_state=self.profile_context.profile.state)
+        else:
+            # If I'm a good match in a safe state, then I must be voting for
+            # a major party, and my friend must be voting for a third party
+            # in a swing state
+            up_friend = self.profile_context.profile
+            return (
+                "Will vote for {up_friend_preferred} in {my_state} in "
+                "exchange for your vote for {up_friend_second} in "
+                "{your_state}").format(
+                    up_friend_preferred=up_friend.preferred_candidate,
+                    my_state=profile.state,
+                    up_friend_second=up_friend.second_candidate,
+                    your_state=up_friend.state)
 
 
 @login_required
