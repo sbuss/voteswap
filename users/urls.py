1d2826c95480f7bcc65b18e0f8669003bb3e9410
Steven Buss <steven.buss@gmail.com>
2016-09-24 22:47:10 -0700

    Add update profile page

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from users.views import profile
 from users.views import propose_swap
+from users.views import update_profile
 
 urlpatterns = [
     url('^profile/$', profile, name='profile'),
     url('^swap/$', propose_swap, name='propose_swap'),
+    url('^profile/update/$', update_profile, name='update_profile'),
 ]
