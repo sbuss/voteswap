c18b9ecbe97a2b7ab222a5272d9bfc9acfa37b1f
Steven Buss <steven.buss@gmail.com>
2016-09-25 16:21:25 -0700

    Add stateface font and a templatetag for it

--- voteswap/settings.py
+++ voteswap/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.django_app.default',
+    'voteswap',
     'polling',
     'users',
     'bootstrap3',
