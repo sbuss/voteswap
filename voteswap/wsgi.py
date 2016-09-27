94e70a0958f0db737ca82c5ea09528bf4e5e4fef
Steven Buss <steven.buss@gmail.com>
2016-09-26 18:02:20 -0700

    Add vendor dir to path

--- voteswap/wsgi.py
+++ voteswap/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voteswap.settings")
 
 application = get_wsgi_application()
+
+try:
+    from google.appengine.ext import vendor
+    vendor.add('lib')
+except ImportError:
+    pass
