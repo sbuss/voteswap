8a66e7dbea5594a098a012fb33507e29199f888b
Steven Buss <steven.buss@gmail.com>
2016-09-28 23:02:34 -0700

    Add polling and users to admin

--- polling/admin.py
+++ polling/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from polling.models import State
+
+admin.site.register(State)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import PairProposal
+from users.models import Profile
+
+admin.site.register(PairProposal)
+admin.site.register(Profile)
