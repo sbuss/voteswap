693cb71e1c26475c11f21d3d2bb96f6edcbfc839
Steven Buss <steven.buss@gmail.com>
2016-10-02 15:30:10 -0700

    Add a verbose formatter

--- voteswap/settings.py
+++ voteswap/settings.py
@@ -145,10 +145,20 @@ else:
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s][%(levelname)s::%(module)s] %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
         },
     },
     'loggers': {
