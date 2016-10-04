fed32989b2fff11e5786c834f8c2f2b05551752e
Steven Buss <steven.buss@gmail.com>
2016-10-03 19:54:33 -0700

    Use jquery-confirm

--- voteswap/static/javascript.js
+++ voteswap/static/javascript.js
@@ -1,23 +1,30 @@
 $(function() {
   $(".js-propose-swap").click(function() {
     var $button = $(this);
+    debugger;
     var userId = $button.data().profileId;
     var name = $button.data().name;
+    $.confirm({
+      title: 'Propose swap?',
+      content: "Are you sure you want to propose a swap with " + name + "?",
+      confirmButton: "Yes!",
+      cancelButton: "Cancel",
+      confirm: function(){
+        $button.prop("disabled", true);
 
-    var conf = confirm("Are you sure you want to propose a swap with " + name + "?");
-
-    if (conf) {
-      $button.prop("disabled", true);
-
-      $.post("/user/swap/", { to_profile: userId, csrfmiddlewaretoken: vs.csrf })
-        .done(function() {
-          $button.parents(".js-swap-option").addClass("proposed");
-        })
-        .fail(function() {
-          $button.prop("disabled", false);
-          alert("Unfortunately we were not able to confirm that swap. Please try again");
-        });
-    }
-
+        $.post("/user/swap/",
+               { to_profile: userId, csrfmiddlewaretoken: vs.csrf })
+          .done(function() {
+            $button.parents(".js-swap-option").addClass("proposed");
+          })
+          .fail(function() {
+            $button.prop("disabled", false);
+            $.alert("Unfortunately we were not able to confirm that swap. " +
+                    "Please try again");
+          });
+      },
+      cancel: function(){
+      }
+    });
   });
 });
