9fd2460e6c821567b2abfa7d9c029d32df5e4611
Steven Buss <steven.buss@gmail.com>
2016-10-15 18:33:39 -0700

    Update version to 2016-10-15

--- voteswap/settings.py
+++ voteswap/settings.py
@@ -52,6 +52,7 @@ ALLOWED_HOSTS = [
     '2016-10-09-dot-voteswap-142902.appspot.com',
     '2016-10-11-dot-voteswap-142902.appspot.com',
     '2016-10-13-dot-voteswap-142902.appspot.com',
+    '2016-10-15-dot-voteswap-142902.appspot.com',
 ]
 
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
