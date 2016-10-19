a42710a4eacf4dc8ab99b47bdf7badff5b2e89b3
Steven Buss <steven.buss@gmail.com>
2016-10-19 08:42:45 -0700

    occupy specific image

--- voteswap/urls.py
+++ voteswap/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     url('^$', landing_page, name='landing_page'),
     url('^gary-johnson/$', landing_page, {'gary': True}, name='landing_page'),
     url('^ows/$', landing_page, {'ows': True}, name='landing_page'),
+    url('^occupy/$', landing_page, {'occupy': True}, name='landing_page'),
     url('^logout/$', logout, name='logout'),
     url('^about/$', about, name='about'),
     url('^press/$', press, name='press'),
