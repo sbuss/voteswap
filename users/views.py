76ed674fc5a63bd864240313166693f1a59c786b
Steven Buss <steven.buss@gmail.com>
2016-11-07 18:43:44 -0800

    Add fb-message-problem email

--- users/views.py
+++ users/views.py
@@ -300,6 +300,34 @@ def _send_bugfix_email(user):
         raise
 
 
+def _send_fb_message_email(user):
+    if not user:
+        logger.info("Skipping nonexistent user")
+        return
+    logger.info("Sending fb-message email to %s", user)
+    if not user.email:
+        logger.error("User %s (%s) doesn't have an email", user, user.id)
+        return
+    message = EmailMultiAlternatives(
+        from_email=u'noreply@email.voteswap.us',
+        to=[user.email],
+        subject=u"Problems contacting your swap? Facebook might be hiding your messages.")  # nopep8
+    message.body = _format_email(
+        render_to_string('users/emails/fb-message-problem.txt'))
+    message.attach_alternative(
+        _format_email(
+            render_to_string('users/emails/fb-message-problem.html')),
+        'text/html')
+    try:
+        message.send()
+    except Exception as e:
+        logger.exception(
+            ("Failed to send fb-message email for user %s. "
+             "Errors: %s"),
+            user, e)
+        raise
+
+
 @login_required
 def propose_swap(request):
     logger.info("%s is propsing a swap", request.user)
